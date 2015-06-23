c4eaa4a43bb4ade44ae734d9d74093ee9faf1503
Mark Egan-Fuller <m.egan-fuller@qmul.ac.uk>
2015-06-23 11:01:04 +0100

    Add swap and tmpfs monitoring and fix colour.

--- i3/mem.py
+++ i3/mem.py
@@ -7,15 +7,43 @@ class Py3status:
     """
 
     def free(self, json, i3status_config):
-        response = {'full_text': '', 'name': 'free'}
+        response = {'full_text': '', 'name': 'ram'}
         total = psutil.phymem_usage().percent
-        if total < 50:
-            response['color'] = i3status_config['color_good']
-        elif total < 70:
-            response['color'] = i3status_config['color_degraded']
-        else:
+        if total < 1:
             response['color'] = i3status_config['color_bad']
+        elif total < 50:
+            response['color'] = i3status_config['color_good']
+        else:
+            response['color'] = i3status_config['color_degraded']
 
         response['full_text'] = "M: %d%%" % total
+
+        return (-2, response)
+
+    def swap(self, json, i3status_config):
+        response = {'full_text': '', 'name': 'swap'}
+        total = psutil.swap_memory().percent
+        if total < 1:
+            response['color'] = i3status_config['color_bad']
+        elif total < 50:
+            response['color'] = i3status_config['color_good']
+        else:
+            response['color'] = i3status_config['color_degraded']
+
+        response['full_text'] = "S: %d%%" % total
+
+        return (-2, response)
+
+    def tempfs(self, json, i3status_config):
+        response = {'full_text': '', 'name': 'tempfs'}
+        total = psutil.disk_usage('/home/markeganfuller/VirtualBoxVMs/').percent
+        if total < 1:
+            response['color'] = i3status_config['color_bad']
+        elif total < 50:
+            response['color'] = i3status_config['color_good']
+        else:
+            response['color'] = i3status_config['color_degraded']
+
+        response['full_text'] = "T: %d%%" % total
 
         return (-2, response)

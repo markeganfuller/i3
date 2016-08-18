a6306e16c99be330951220ba95cfb3bdc5d3ed06
Mark Egan-Fuller <m.egan-fuller@qmul.ac.uk>
2016-08-18 14:47:24 +0100

    Tidy up.

--- i3/i3statusconf.py
+++ i3/i3statusconf.py
@@ -1,3 +1,4 @@
+import netifaces
 import i3pystatus
 import keyboardmap
 import mem
@@ -14,25 +15,22 @@ color_off = "#333333"
 status.register("clock", format="%Y-%m-%d %H:%M:%S %z %s")
 status.register(keyboardmap.KeyboardMap, color_good=color_ok)
 status.register(temps.OnboardTemp, color_norm=color_ok)
-status.register(temps.WeatherTemp, color_norm=color_ok)
 status.register("load", format="{avg1} {avg5} {avg15}",
                 critical_color=color_bad)
-status.register(mem.TempfsFree, color_up=color_good, color_down=color_off,
-                color_critical=color_bad)
 status.register(mem.SwapFree, color_up=color_ok, color_down=color_off,
                 color_critical=color_bad)
 status.register(mem.MemFree, color_up=color_ok, color_down=color_off,
                 color_critical=color_bad)
-status.register(vms, color_up=color_good, color_down=color_off)
-status.register("runwatch", name="DHCP", path="/var/run/dhcpcd*.pid",
-                color_up=color_ok, color_down=color_off)
+status.register(vms.VMs, color_up=color_good, color_down=color_off)
 status.register("network", interface="net0", format_up="E:{v4}",
                 format_down="E", color_up=color_ok, color_down=color_off,
                 on_leftclick=None, on_rightclick=None, on_upscroll=None,
                 on_downscroll=None)
-status.register("network", interface="wnet0", format_up="W:{v4}",
-                format_down="W", color_up=color_ok, color_down=color_off,
-                on_leftclick=None, on_rightclick=None, on_upscroll=None,
-                on_downscroll=None)
+
+if "wnet0" in netifaces.interfaces():
+    status.register("network", interface="wnet0", format_up="W:{v4}",
+                    format_down="W", color_up=color_ok, color_down=color_off,
+                    on_leftclick=None, on_rightclick=None, on_upscroll=None,
+                    on_downscroll=None)
 
 status.run()
--- i3/mem.py
+++ i3/mem.py
@@ -25,7 +25,7 @@ class MemFree(i3pystatus.IntervalModule):
         else:
             response['color'] = self.color_critical
 
-        response['full_text'] = "M: %d%%" % total
+        response['full_text'] = "M:%d%%" % total
         self.output = response
 
 
@@ -50,31 +50,5 @@ class SwapFree(i3pystatus.IntervalModule):
         else:
             response['color'] = self.critical_color
 
-        response['full_text'] = "S: %d%%" % total
-        self.output = response
-
-
-class TempfsFree(i3pystatus.IntervalModule):
-    color_up = "#00F000"
-    color_critical = "#E50000"
-    color_down = "#333333"
-    interval = 60
-    settings = (
-        ("color_up", ""),
-        ("color_critical", ""),
-        ("color_down", "")
-    )
-
-    def run(self):
-        response = {'full_text': '', 'name': 'tempfs'}
-        path = os.path.expanduser('~/VirtualBoxVMs/')
-        total = psutil.disk_usage(path).percent
-        if total < 1:
-            response['color'] = self.color_down
-        elif total < 50:
-            response['color'] = self.color_up
-        else:
-            response['color'] = self.color_critical
-
-        response['full_text'] = "T: %d%%" % total
+        response['full_text'] = "S:%d%%" % total
         self.output = response
